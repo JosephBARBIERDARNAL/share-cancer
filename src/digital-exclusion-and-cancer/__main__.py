5c84bf2d46cd296f9c40ad75bd7072d619c85da8
Barbier--Darnal Joseph <joseph.barbierdarnal@gmail.com>
2026-05-02 15:58:27 +0200

    autopush

--- src/digital-exclusion-and-cancer/__main__.py
+++ src/digital-exclusion-and-cancer/__main__.py
@@ -1,2 +1,140 @@
-# import pyshare as ps
-# import polars as pl
+import pyshare as ps
+import polars as pl
+
+from utils import (
+    ENDS_MEET,
+    COMPUTER_SKILLS,
+    GENDER,
+    HEALTH_LITERACY_HELP,
+    ISCED_1997,
+    clean_share_missing,
+    recode_yes_no,
+)
+
+
+df = ps.read_share_wave(
+    wave=9,
+    path="../data-share",
+    modules=[
+        "cv_r",
+        "dn",
+        "ph",
+        "hc",
+        "it",
+        "hh",
+        "co",
+        "gv_isced",
+        "gv_health",
+        "gv_weights",
+    ],
+)
+
+df_clean = (
+    df.select(
+        "mergeid",
+        "country",
+        "hhid9",
+        "gender",
+        "dn003_",
+        "ph006d10",
+        "hc886_",
+        "hc887_",
+        "hc876_",
+        "hc877_",
+        "hc889_",
+        "it003_",
+        "it004_",
+        "hh017e",
+        "co007_",
+        "isced1997_r",
+        "isced2011_r",
+        "adl",
+        "bmi",
+        "cchw_w9",
+    )
+    .rename(
+        {
+            "dn003_": "year_of_birth",
+            "ph006d10": "cancer",
+            "hc886_": "mammogram",
+            "hc887_": "colon_cancer_screening",
+            "hc876_": "gp_contacts",
+            "hc877_": "specialist_contacts",
+            "hc889_": "health_literacy_help",
+            "it003_": "computer_skills",
+            "it004_": "internet_past_7_days",
+            "hh017e": "hh_monthly_income",
+            "co007_": "make_ends_meet",
+            "isced1997_r": "isced1997",
+            "isced2011_r": "isced2011",
+            "cchw_w9": "cross_sectional_weight",
+        }
+    )
+    .with_columns(
+        country=pl.col("country").replace_strict(
+            ps.MAP_ID_TO_COUNTRY, return_dtype=pl.Utf8
+        ),
+        gender=clean_share_missing(pl.col("gender")).replace_strict(
+            GENDER, default=None, return_dtype=pl.Utf8
+        ),
+        age=(2022 - clean_share_missing(pl.col("year_of_birth"))),
+        cancer=recode_yes_no("cancer"),
+        mammogram=recode_yes_no("mammogram"),
+        colon_cancer_screening=recode_yes_no("colon_cancer_screening"),
+        internet_past_7_days=recode_yes_no("internet_past_7_days"),
+        computer_skills=clean_share_missing(pl.col("computer_skills")).replace_strict(
+            COMPUTER_SKILLS, default=None, return_dtype=pl.Utf8
+        ),
+        health_literacy_help=clean_share_missing(
+            pl.col("health_literacy_help")
+        ).replace_strict(HEALTH_LITERACY_HELP, default=None, return_dtype=pl.Utf8),
+        make_ends_meet=clean_share_missing(pl.col("make_ends_meet")).replace_strict(
+            ENDS_MEET, default=None, return_dtype=pl.Utf8
+        ),
+        isced1997=clean_share_missing(pl.col("isced1997")),
+        isced1997_label=clean_share_missing(pl.col("isced1997")).replace_strict(
+            ISCED_1997, default=None, return_dtype=pl.Utf8
+        ),
+        isced2011=clean_share_missing(pl.col("isced2011")),
+        gp_contacts=clean_share_missing(pl.col("gp_contacts")),
+        specialist_contacts=clean_share_missing(pl.col("specialist_contacts")),
+        hh_monthly_income=clean_share_missing(
+            pl.col("hh_monthly_income"), financial=True
+        ),
+        adl=clean_share_missing(pl.col("adl")),
+        bmi=clean_share_missing(pl.col("bmi")),
+        cross_sectional_weight=clean_share_missing(pl.col("cross_sectional_weight")),
+    )
+    .with_columns(
+        low_computer_skill=pl.col("computer_skills").is_in(
+            ["Poor", "Never used a computer"]
+        ),
+        no_recent_internet=pl.col("internet_past_7_days").eq(False),
+        digitally_excluded=(
+            pl.col("computer_skills").is_in(["Poor", "Never used a computer"])
+            | pl.col("internet_past_7_days").eq(False)
+        ),
+        tertiary_educated=pl.col("isced1997").is_in([5, 6]),
+        log_hh_monthly_income=pl.col("hh_monthly_income").log1p(),
+        any_screening=pl.any_horizontal(
+            pl.col("mammogram").eq(True), pl.col("colon_cancer_screening").eq(True)
+        ),
+    )
+    .with_columns(
+        eligible_colon_screening=pl.col("age").is_between(50, 74),
+        eligible_mammogram=(
+            pl.col("gender").eq("Female") & pl.col("age").is_between(50, 69)
+        ),
+    )
+    .with_columns(
+        missed_colon_screening=(
+            pl.col("eligible_colon_screening")
+            & pl.col("colon_cancer_screening").eq(False)
+        ),
+        missed_mammogram=(pl.col("eligible_mammogram") & pl.col("mammogram").eq(False)),
+    )
+    .filter(pl.col("age").is_between(50, 100))
+)
+
+df_clean.head()
+df_clean.describe()
