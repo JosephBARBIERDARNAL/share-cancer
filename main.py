e888e29432f14caf5d1a946ba3b47e83a6ab7317
Barbier--Darnal Joseph <joseph.barbierdarnal@gmail.com>
2026-05-02 15:26:05 +0200

    autopush

--- main.py
+++ main.py
@@ -1,6 +1,34 @@
-def main():
-    print("Hello from share-cancer!")
+import pyshare as ps
+import polars as pl
+import morethemes as mt
+
+mt.set_theme("minimal")
 
 
-if __name__ == "__main__":
-    main()
+df = ps.read_share_wave(
+    wave=9, path="../data-share", modules=["cv_r", "ph", "dn", "ep"]
+)
+
+df_clean = df.select(
+    "mergeid",
+    "country",
+    "gender",
+    "hhid9",
+    "exrate",
+    receive_recognition="ep032_",
+    year_of_birth="dn003_",
+    cancer="ph006d10",
+).with_columns(
+    country=pl.col("country").replace_strict(
+        ps.MAP_ID_TO_COUNTRY, return_dtype=pl.Utf8
+    ),
+    age=(2022 - pl.col("year_of_birth")),
+    receive_recognition=pl.col("receive_recognition").replace_strict(
+        {
+            1: "Strongly agree",
+            2: "Agree",
+            3: "Disagree",
+            4: "Strongly disagree",
+        }
+    ),
+)
